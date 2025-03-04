d1d85c47dfac9860f33a23f7d1058c3ef02aaeaa
Upasana <officialupasana75@gmail.com>
2025-03-04 20:41:29 +0530

    Update MAZE.py

--- MAZE.py
+++ MAZE.py
@@ -1,6 +1,10 @@
-from pyamaze import maze
+from pyamaze import maze, COLOR, agent
 
-m=maze(15,16)    #Determining the size of the maze
+m=maze(7,20)    #Determining the size of the maze
 m.CreateMaze(loopPercent=40)   #Creating the maze
-m.run() #Running the application
 
+a=agent(m,shape='square',footprints=True,filled=True)
+
+m.tracePath({a:m.path},delay=100, showMarked=True)
+
+m.run() #Running the application
