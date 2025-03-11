401a1068d7ea0cb4c41df24ce7e3ee32941ee0fd
martinss27 <leoalmeidamartins@gmail.com>
2025-03-11 18:17:08 -0300

    Improved DFS algorithm

--- MAZE.py
+++ MAZE.py
@@ -6,38 +6,53 @@ def DFS(m):
     frontier=[start]
     logicpath={}
 
+    print("Maze structure:", m.maze_map)
 
-    while len(frontier)>0:
-        current=frontier.pop() #To move to next position on map, the values gathered in stack of frontier is used
-        if current==(1,1):
+    while frontier:
+        current = frontier.pop()
+        print(f"Exploring: {current}")
+
+        if current == (1,1):
             break
+
         for d in 'ESNW': # priority is WNSE, but put in reverse as it uses stack
-            if m.maze_map[current][d]==True:
+            if m.maze_map[current][d]:
                 if d == 'E':
-                    next = (current[0], current[1] + 1)
-                elif d== 'W':
-                    next = (current[0], current[1] - 1)
+                    next_cell = (current[0], current[1] + 1)
+                elif d == 'W':
+                    next_cell = (current[0], current[1] - 1)
                 elif d == 'S':
-                    next = (current[0] + 1, current[1])
+                    next_cell = (current[0] + 1, current[1])
                 elif d == 'N':
-                    next = (current[0] - 1, current[1])
+                    next_cell = (current[0] - 1, current[1])
+
                 if next in seen:
                     continue
-                seen.append(next)
-                frontier.append(next)
-                logicpath[next]=current #to map path from goal to the start
+
+                seen.append(next_cell)
+                frontier.append(next_cell)
+                logicpath[next_cell] = current #to map path from goal to the start
+    
+    if (1,1) not in logicpath:
+        print('Error: DFS did not find a valid path.')
+        return {}
+    
     fwd_path={}
     cell=(1,1)
     while cell != start:
-        fwd_path[logicpath[cell]]=cell
+        fwd_path[logicpath[cell]] = cell
         cell=logicpath[cell]
+        
         return fwd_path
 
 if __name__=='__main__':
     m=maze(7,25)    #Determining the size of the maze
     m.CreateMaze(loopPercent=40)   #Creating the maze
+    
     path= DFS(m)
-    a=agent(m,shape='arrow', footprints=True)
-    m.tracePath({a:m.path},delay=100, showMarked=True)
-
-    m.run()
+    if path:
+        a=agent(m,shape='arrow', footprints=True)
+        m.tracePath({a: path}, delay=100, showMarked=True)
+        m.run()
+    else:
+        print('No path found. Finishing execution')
